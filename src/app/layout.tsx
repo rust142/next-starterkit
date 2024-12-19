11ce2e1fc5ec04f2eb1342743123b1cc00299443
Agung Maulana <amdev142@gmail.com>
2024-12-19 19:07:00 +0700

    refactor(layout): remove unnecessary jsx tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   )
 }
